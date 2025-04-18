043b51ff5373811d356df972b2b80f275884561e
silvermoon <85053204+ejseo87@users.noreply.github.com>
2025-04-18 22:35:10 +0900

    7.6 Uploading Documents

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -1,30 +1,59 @@
 import streamlit as st
-import time
+import os
+from langchain.storage import LocalFileStore
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.embeddings import OpenAIEmbeddings, CacheBackedEmbeddings
+from langchain.vectorstores import FAISS
 
 st.set_page_config(
     page_title="DocumentGPT",
     page_icon=":memo:",
 )
+
+
+def embed_file(file):
+    file_content = file.read()
+    file_path = os.path.join(".cache", "files", file.name)
+    # Save the file
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+    st.success(f"File [{file.name}] uploaded successfully!")
+    cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
+    splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        separator="\n",
+        chunk_size=600,
+        chunk_overlap=100,
+    )
+    loader = UnstructuredFileLoader("./files/chapter_one.txt")
+    docs = loader.load_and_split(text_splitter=splitter)
+    embeddings = OpenAIEmbeddings()
+    cached_embeddings = CacheBackedEmbeddings.from_bytes_store(
+        embeddings, cache_dir)
+    vectorstore = FAISS.from_documents(
+        documents=docs,
+        embedding=cached_embeddings
+    )
+    retriever = vectorstore.as_retriever()
+    return retriever
+
+
 st.title("DocumentGPT")
 
-if "messages" not in st.session_state:
-    st.session_state["messages"] = []
+st.markdown("""
+Welcome!
 
+Use this chatbot to ask questions to an AI about your file!
+""")
 
-def send_message(message, role, save=True):
-    with st.chat_message(role):
-        st.write(message)
-        if save:
-            st.session_state["messages"].append(
-                {"role": role, "message": message})
+# Create necessary directories
+os.makedirs("./.cache/files", exist_ok=True)
+os.makedirs("./.cache/embeddings", exist_ok=True)
 
+file = st.file_uploader("Upload a .txt .pdf or .docx file", type=[
+                        "pdf", "docx", "txt"])
 
-for message in st.session_state["messages"]:
-    send_message(message["message"], message["role"], save=False)
-
-
-message = st.chat_input("Send a message to the AI")
-if message:
-    send_message(message, "human")
-    time.sleep(2)
-    send_message("You said: " + message, "ai")
+if file:
+    retriever = embed_file(file)
+    docs = retriever.invoke("whale")
+    st.write(docs)
