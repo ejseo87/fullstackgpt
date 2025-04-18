d4e532aee63997bf09da0a564ef46eb975fea1c5
silvermoon <85053204+ejseo87@users.noreply.github.com>
2025-04-18 10:31:41 +0900

    7.2 Data Flow

--- home.py
+++ home.py
@@ -1,9 +1,23 @@
 import streamlit as st
-from langchain.prompts import PromptTemplate
-a = [1, 2, 3, 4]
-d = {"a": 1, "b": 2}
-p = PromptTemplate.from_template("xxxxx")
-a
-d
-p
-st.selectbox("select a number", ("GPT-4", "GPT-3.5"))
+from datetime import datetime
+
+today = datetime.today().strftime("%H:%M:%S")
+st.title(today)
+model = st.selectbox("select a number", ("GPT-4", "GPT-3.5"))
+
+if model == "GPT-3.5":
+    st.write("cheap")
+else:
+    st.write("expensive")
+    name = st.text_input("Enter your name")
+    st.write(name)
+
+
+    value = st.slider("temperature", min_value=0.1, max_value=1.0)
+    st.write(value)
+
+    if st.button("Submit"):
+        st.write(f"Hello {name}")
+
+
+
